f380f1676dfcadae28f56d4d1eef46ae036913b3
yedidya katz <yedidyakatz@MacBook-Air-sl-yedidya.local>
2025-09-10 02:01:23 +0300

    remoev dist

--- dist/sharedInstances.d.ts
+++
@@ -1,7 +0,0 @@
-import { TelegramInstance } from './telegramInstance';
-import { WhatsAppInstance } from './whatsappInstance';
-import ForwardingManager from './forwardingManager';
-export declare const telegramInstance: TelegramInstance;
-export declare const whatsappInstance: WhatsAppInstance;
-export declare const forwardingManager: ForwardingManager;
-//# sourceMappingURL=sharedInstances.d.ts.map
--- dist/telegramApi.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export default router;
-//# sourceMappingURL=telegramApi.d.ts.map
